f888b2dcc80c14dad6e91f01dc0d030921c9ed99
gamea <gamea@10.159.1.143>
2017-10-09 19:56:55 +0200

    Ready to test

--- AP/src/AP.java
+++ AP/src/AP.java
@@ -9,39 +9,56 @@ public class AP {
 	
 	private Word word;
 	
+	private ArrayList<Word> wordRegister;
 	private ArrayList<Transition> transitionStack;
 	private State currentState;
 	
-	public AP() {
-		
+	public AP(ArrayList<NoTerminal> noTerminales, ArrayList<Terminal> terminales, ArrayList<State> states, ArrayList<Transition> allTransitions, Word word {
+		this.noTerminales = noTerminales;
+		this.terminales = terminales;
+		this.states = states;
+		this.allTransitions = allTransitions;
+		this.word = word;
+		this.stack = new Stack();
+		this.wordRegister = new ArrayList<Word>();
+		this.transitionStack = new ArrayList<Transition>();
 	}
 	public void step() {
 		// Guardamos todas las transiciones posibles actualmente y su estado actual
 		for(Transition i: getAllTransitions()) {
-			if(i.hasPath(getCurrentState(), getWord(), (NoTerminal) getStack().seeTop()))
+			if(i.hasPath(getCurrentState(), getWord(), (NoTerminal) getStack().seeTop())) {
 				getTransitionStack().add(i);
+				getWordRegister().add(getWord());
 		}
 		if(!getTransitionStack().isEmpty()) {
-			
+			Transition transicion = getTransitionStack().get(getTransitionStack().size() - 1);
 			// Realizamos la última transición disponible
 			// Si es posible una transición actual
-						
+			setCurrentState(transicion.getPrevState());
+			System.out.println(transicion);
 			if(getTransitionStack().get(getTransitionStack().size() - 1).hasPath(getCurrentState(), getWord(), (NoTerminal) getStack().seeTop())) {
-							
+				/**
+				 * Volver al estado y a la palabra
+				 * Setear el estado nuevo
+				 * Borrar carácter
+				 * Borrar pila y añadir pila
+				 */
+				setWord(getWordRegister().get(getWordRegister().size() - 1));
+				setCurrentState(transicion.getNextState());
+				// Borramos el terminal
+				word.delete();
+				// Borramos el no terminal
+				stack.pop();
+				// Añadimos no terminales
+				ArrayList<NoTerminal> toInsert = getTransitionStack().get(getTransitionStack().size() - 1).getNextNoTerminal();
+				for(int i = toInsert.size() - 1; i >= 0; i--) {
+					stack.push(toInsert.get(i));
+				}
 			}
 			
 			// Realizamos la transicion
 			// Seteamos estado a siguiente
-			setCurrentState(getTransitionStack().get(getTransitionStack().size() - 1).getNextState());
-			// Borramos el terminal
-			word.delete();
-			// Borramos el no terminal
-			stack.pop();
-			// Añadimos no terminales
-			ArrayList<NoTerminal> toInsert = getTransitionStack().get(getTransitionStack().size() - 1).getNextNoTerminal();
-			for(int i = toInsert.size() - 1; i >= 0; i--) {
-				stack.push(toInsert.get(i));
-			}
+			
 		}
 	}
 	
@@ -65,6 +82,9 @@ public class AP {
 	}
 	public Word getWord() {
 		return this.word;
+	}
+	public ArrayList<Word> getWordRegister() {
+		return this.wordRegister;
 	}
 	public void setNoTerminales(ArrayList<NoTerminal> noTerminales) {
 		this.noTerminales = noTerminales;
